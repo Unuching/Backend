dc5f3f2d2ccca47e99cdec1afc37120e8faf6f2d
Unuching Marma <unuching.marma2023@gmail.com>
2025-06-02 09:34:44 +0600

    add backend file

---
+++ one_server/node_server.js
@@ -0,0 +1,23 @@
+const { log } = require("console");
+const http = require("http");
+const hostName = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/plain");
+    res.end("Hello from Unu");
+  } else if (req.url === "/tea") {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/plain");
+    res.end ("Thamks for ordering,its really hot");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-type", "text/plain");
+    res.end("404 Not Found");
+  }
+});
+server.listen(port, hostName, () => {
+  console.log(`Server is listening at http://${hostName}:${port}`);
+});
